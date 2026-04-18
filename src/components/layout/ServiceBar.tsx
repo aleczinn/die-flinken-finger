e3e86d7223bbce9d5159421a295655f1fa3aaa40
Alec <alec_z17@web.de>
2026-04-18 15:26:33 +0200

    adjust service bar with labels etc.

--- src/components/layout/ServiceBar.tsx
+++ src/components/layout/ServiceBar.tsx
@@ -11,23 +11,26 @@ interface ServiceBarProps {
 export default async function ServiceBar({ locale }: ServiceBarProps) {
 	const config = await getGlobalConfig(locale);
 	const telephone = config.telephone.replace(/\s/g, '');
+	const email = config.email;
 
 	return (
 		<div className="bg-gray-90 text-white">
-			<Section as="nav" variant="capped" className="flex flex-row justify-center sm:justify-end items-center py-2" aria-label={t(locale, 'header.service_bar')}>
+			<Section as="nav" variant="capped" className="flex flex-row justify-center sm:justify-end items-center py-2" aria-label={t(locale, 'header.service_bar.label')}>
 				<div className="flex flex-row gap-4">
 					<a href={`tel:${telephone}`}
 					   className="flex flex-row items-center gap-2 text-white transition-colors duration-100 hover:text-gray-20 hover:cursor-pointer"
+					   aria-label={t(locale, 'header.service_bar.telephone', telephone)}
 					>
 						<IconTelephone className="w-4 h-4"/>
-						<span>0123 4567890</span>
+						<span>{telephone}</span>
 					</a>
 
-					<a href={`mailto:${config.email}`}
+					<a href={`mailto:${email}`}
 					   className="flex flex-row items-center gap-2 text-white transition-colors duration-100 hover:text-gray-20 hover:cursor-pointer"
+					   aria-label={t(locale, 'header.service_bar.email', email)}
 					>
 						<IconMail className="w-4 h-4"/>
-						<span>info@flinke-finger.de</span>
+						<span>{email}</span>
 					</a>
 				</div>
 			</Section>
