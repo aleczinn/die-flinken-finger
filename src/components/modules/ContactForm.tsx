014a45044e6204a90d98ad415f2377baae7d0bb3
Alec <alec_z17@web.de>
2026-04-24 13:05:21 +0200

    implement base for contact form

---
+++ src/components/modules/ContactForm.tsx
@@ -0,0 +1,13 @@
+import { Locale } from "@/lib/locale/locales";
+
+interface ContactFormProps {
+    locale: Locale
+}
+
+export default function ContactForm({ locale }: ContactFormProps) {
+    return (
+        <div className="bg-white">
+            form
+        </div>
+    );
+}
